e657c5651c08ce121471085942f9ebad96a909f5
namdang-fdp <namdangcoder@gmail.com>
2025-12-02 11:39:23 +0700

    feat: add leetcode 3 solution

---
+++ excercises/longest_substring_without_repeating.go
@@ -0,0 +1,34 @@
+// this is leetcode 003 problem
+package main
+
+// abcabcbb
+// pwwkew
+func lengthOfLongestSubstring(s string) int {
+	freq := make(map[byte]int)
+	left := 0
+	maxLen := 0
+
+	for right := 0; right < len(s); right++ {
+		c := s[right]
+		freq[c]++
+
+		for freq[c] > 1 {
+			leftChar := s[left]
+			freq[leftChar]--
+			left++
+		}
+		subStringLen := right - left + 1
+		if subStringLen > maxLen {
+			maxLen = subStringLen
+		}
+	}
+	return maxLen
+}
+
+func solve_03() {
+
+}
+
+func main() {
+	solve_03()
+}
